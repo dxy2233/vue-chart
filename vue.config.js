bc663d9453d3aca1a89e72b617abbba44cc28952
dxy2233 <691369338@qq.com>
2019-02-12 20:38:23 +0800

    配置打包观察

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,18 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 module.exports = {
   devServer: {
     overlay: {
         warnings: true,
         errors: true
+    }
+  },
+  configureWebpack: config => {
+    if (process.env.npm_lifecycle_event === 'build2') {
+      return {
+        plugins: [
+          new BundleAnalyzerPlugin()
+        ]
+      }
     }
   }
 }
