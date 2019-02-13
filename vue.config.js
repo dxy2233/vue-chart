de3d931fc0c9d6da551ba1fcb33e7c041355f386
dxy2233 <691369338@qq.com>
2019-02-13 15:05:20 +0800

    更新环境修改图标

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
 module.exports = {
   devServer: {
     overlay: {
@@ -6,7 +7,26 @@ module.exports = {
         errors: true
     }
   },
+  chainWebpack: config => {
+    if (true) {
+      config.plugin('html')
+      .tap(args => {
+        args[0].title = 'yy聊天室'
+        if (process.env.npm_lifecycle_event !== 'serve') {
+          args[0].cdnvue = '<script src="https://cdn.bootcss.com/vue/2.6.3/vue.runtime.min.js"></script>'
+          args[0].cdnrouter = '<script src="https://cdn.bootcss.com/vue-router/3.0.2/vue-router.min.js"></script>'
+        }
+        return args
+      })
+    }
+  },
   configureWebpack: config => {
+    if (process.env.npm_lifecycle_event !== 'serve') {
+      config.externals = {
+        'vue': 'Vue',
+        'vue-router': 'VueRouter'
+      }
+    }
     if (process.env.npm_lifecycle_event === 'build2') {
       return {
         plugins: [
@@ -14,5 +34,6 @@ module.exports = {
         ]
       }
     }
-  }
+  },
+  productionSourceMap: false
 }
